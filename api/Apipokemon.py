8d9a50f6a9799527f90bd857ffc58f9d3e7571ac
DanielIibarra <noe.gallegosib@uanl.edu.mx>
2022-06-19 10:44:48 -0500

    cambios

--- api/Apipokemon.py
+++ api/Apipokemon.py
@@ -1,5 +1,15 @@
 from tkinter import Tk, Label, Button
 import tkinter
+import urllib.request,urllib.parse,urllib.error
+import json
+import ssl
+
+ctx = ssl.create_default_context()
+ctx.check_hostname= False
+ctx.verify_mode= ssl.CERT_NONE
+
+api = "https://pokeapi.co/api/v2/pokemon/"
+
 
 class Pokedex:
 
@@ -12,12 +22,17 @@ class Pokedex:
         self.dato=tkinter.StringVar()
         self.entrada=tkinter.Entry(master,textvariable=self.dato)
         self.entrada.pack()
-        self.boton=Button(master,text="Buscar",command=self.salidaTexto)
+        self.boton=Button(master,text="Buscar",command=self.buscar)
         self.boton.pack()
         
-    def salidaTexto(self):
-        print(self.dato.get())
-    
+    def buscar(self):
+        url=api+self.dato.get()
+        print(url)
+
+        self.web=urllib.request.urlopen(url)
+        self.datos=self.web.read().decode()
+        print('Retrieved', len(self.datos), 'characters')
+
 
 root=Tk()
 Pokedex(root)
---
+++ prueba.py
@@ -0,0 +1,8 @@
+import urllib.request, urllib.parse, urllib.error
+import json
+
+api="https://pokeapi.co/api/v2/pokemon/"
+
+while True:
+    pokemon=input("Pokemon: ")
+    if len(pokemon)<1: break
